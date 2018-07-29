800a495b7da233a9c6f424f394efe2490c56db13
Maksym Koldun <koldun.maks@gmail.com>
2018-07-29 03:04:50 +0300

    first test with checking validatorsErrors length

--- src/CartParser.test.js
+++ src/CartParser.test.js
@@ -9,6 +9,12 @@ beforeEach(() => {
 describe("CartParser - unit tests", () => {
     // Add your unit tests here.
 
+    test("should throw exception if exist validation errors (means exist > 0)", () => {
+        let content = 'Product name,Price,Quantity';
+        let validationErrors = parser.validate(content)
+
+        expect(validationErrors.length).toEqual(0);
+    });
 });
 
 describe("CartParser - integration tests", () => {
